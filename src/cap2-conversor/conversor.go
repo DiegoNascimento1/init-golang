44a194fbe06b19f39ecf6f70e35b2c4fb0d31095
DiegoNascimento1 <diegoh4money@gmail.com>
2022-10-28 09:52:01 -0300

    feat: criando cornversor

---
+++ src/cap2-conversor/conversor.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Uso: conversor <valores> <unidade>")
+		os.Exit(1)
+	}
+
+	unidadeOrigem := os.Args[len(os.Args)-1]
+	valoresOrigem := os.Args[1 : len(os.Args)-1]
+
+	var unidadeDestino string
+
+	if unidadeOrigem == "celsius" {
+		unidadeDestino = "fahrenheit"
+	} else if unidadeOrigem == "quilometros" {
+		unidadeDestino = "milhas"
+	} else {
+		fmt.Println("%s não é uma unidade conhecida!",
+			unidadeDestino)
+		os.Exit(1)
+	}
+}
